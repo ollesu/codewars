0b67f77c22463590bc9303214e32a876f8317150
Aisa Mukukenova <mkaysa@gmail.com>
2019-06-15 20:27:45 +0100

    fixed bug

--- 8kyu_final_grade.rb
+++ 8kyu_final_grade.rb
@@ -2,8 +2,8 @@ def final_grade(exam, projects)
   # final grade
   case
   when exam > 90 || projects > 10 then 100
-  when exam > 75 || projects > 5 then 90
-  when exam > 50 || projects >= 2 then 75
+  when exam > 75 &&  projects >= 5 then 90
+  when exam > 50 && projects >= 2 then 75
   else
     0
   end
